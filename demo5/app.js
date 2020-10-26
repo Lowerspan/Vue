d4a91c09c14cf4ac1c5b060d66d75f41e7b20a71
lizonghan <1808874460@qq.com>
2020-10-26 12:20:08 +0800

    基础

---
+++ demo5/app.js
@@ -0,0 +1,29 @@
+//实例化vue对象
+new Vue({
+    el: "#vue-app",
+    data: {
+        age: 20,
+        a: 0,
+        b: 0
+    },
+    methods: {
+        // addToA: function() {
+        //     console.log('Add to A')
+        //     return this.a + this.age;
+        // },
+        // addToB: function() {
+        //     console.log('Add to B')
+        //     return this.b + this.age;
+        // }
+    },
+    computed: {
+        addToA: function() {
+            console.log('Add to A')
+            return this.a + this.age;
+        },
+        addToB: function() {
+            console.log('Add to B')
+            return this.b + this.age;
+        }
+    }
+})
